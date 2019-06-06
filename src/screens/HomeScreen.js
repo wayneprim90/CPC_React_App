f12543e4004a2d1db6f238aba1cb41f179afc4e9
Wil Prim <wayneprim90@gmail.com>
2019-06-06 14:07:10 -0600

    Remove HEAD STUFF WEIRD

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -38,11 +38,7 @@ class HomeScreen extends Component {
             justifyContent: "center"
         }}>
             <StatusBar backgroundColor="#eee" barStyle="dark-content"></StatusBar>
-<<<<<<< HEAD
             <Text>Count: {this.state.counter}</Text>
-=======
-            <Text>Home Screen</Text>
->>>>>>> e2eec9700d45d1f7d159ed26e32fd53273481732
         </View>
         )
     }
