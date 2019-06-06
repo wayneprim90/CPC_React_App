3693a01db0e7aa869fdb5a926993be2f24a9ab5b
Wil Prim <wayneprim90@gmail.com>
2019-06-06 13:49:57 -0600

    Change to background color for styling tab bar

--- App.js
+++ App.js
@@ -18,15 +18,17 @@ const MainAppNavigator = createBottomTabNavigator(
   {
     defaultNavigationOptions: ({navigation}) => ({
       tabBarIcon: ({focused, tintColor, horizontal}) => {
+
         const { routeName } = navigation.state;
         let IconComponent = Ionicons;
         let iconName;
+
         if (routeName === 'Home') { 
           iconName = 'ios-home';
         } else if (routeName === 'Events') {
           iconName = `ios-calendar`;
         } else if (routeName === "Information") {
-          iconName = 'ios-options'
+          iconName = 'ios-options' 
         }
 
         // You can return any component that you like here!
@@ -34,10 +36,11 @@ const MainAppNavigator = createBottomTabNavigator(
       }
     }),
     tabBarOptions: {
-      inactiveBackgroundColor: "#01255D",
-      activeBackgroundColor: "#01255D",
       activeTintColor: "#2DC7E5",
-      inactiveTintColor: "#eef0f0"
+      inactiveTintColor: "#eef0f0",
+      style: {
+        backgroundColor: "#01255D"
+      }
     }
   }
 )
