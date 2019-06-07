bee35c75498099429ca980a1b5a5c47e4c4a2872
Wil Prim <wayneprim90@gmail.com>
2019-06-07 01:09:45 -0600

    Styling AudioModal

--- src/components/modals/AudioModal.js
+++ src/components/modals/AudioModal.js
@@ -24,21 +24,17 @@ class AudioModal extends Component {
                         <View style={{ width: "20%"}}>
                             { this.state.audioObject ? <FeatherIconComponent name="share" size={25} style={{textAlign: "center", color: "#fff"}} /> : null }
                         </View>
-                        <Text style={{ width: "60%", textAlign: "center", color: "#fff", fontSize: 15, fontWeight: "bold" }}>
-                            {this.state.audioObject ? this.state.audioObject.title : "No Audio :("}
-                        </Text> 
+                        
                         <TouchableOpacity onPress={this.props.close} style={{ width: "20%" }}>
                             <FeatherIconComponent name="chevron-down" size={25} style={{ textAlign: "center", color: "#fff"}} /> 
                         </TouchableOpacity>
                     </View>
                     
-                    <View style={{width: "100%", alignItems: "center", justifyContent: "flex-start"}}>
+                    <View style={{width: "100%", alignItems: "center", justifyContent: "flex-start", backgroundColor: "blue"}}>
                         <Image style={{ height: (Dimensions.get('window').width * .8), borderRadius: 10 }} source={require("../../assets/images/album-image.png")} resizeMode="contain" />
                     </View>
 
-                    <View style={{ width: "100%", height: 200 }} >
-
-                    </View>
+                    <View style={{backgroundColor: "blue", width: "100%", height: 200}}></View>
 
                 </SafeAreaView>  
             </Modal> 
