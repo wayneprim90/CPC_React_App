767676953933c702df4e54d3ea9027bc64df1ccf
Wil Prim <wayneprim90@gmail.com>
2019-06-07 03:55:21 -0600

    Audio sermons now in home screen. Audio seeking enabled, audio pause and play enabled!

--- src/components/modals/AudioModal.js
+++ src/components/modals/AudioModal.js
@@ -3,57 +3,67 @@ import { Modal, Text, View, TouchableOpacity, StatusBar, Image, Dimensions} from
 import { SafeAreaView } from 'react-navigation';
 import FeatherIconComponent from "react-native-vector-icons/Feather";
 import Slider from "@react-native-community/slider";
+import Video from "react-native-video";
 
 class AudioModal extends Component {
 
     state = {
-        audioObject : {
-            title: "The Model Prayer", 
-            date: "05-05-2019"
-        },
-        playing: false,
-        timer: {
-            current: 20
-        }
+        audioPaused: false,
+        currentTime: 0,
+        maxTime: 0
     }
 
-    playToggle = () => {
+    
+    loadedAudio = (data) => {
+        //Get minutes of max data
+        this.setState({ maxTime: Math.floor(data.duration) })
+    }
+
+    setProgress = (data) => {
+        this.setState({ currentTime: Math.floor(data.currentTime)});
+    }
+
+    sliderComplete = (time) => {
+        time = Math.floor(time)
+        this.refs.sermonElement.seek(time)
         this.setState({
-            playing: !this.state.playing
+            currentTime: time
         })
     }
 
-    backwardFifteen = () => {
+    togglePlay = () => {
         this.setState({
-            timer: {
-                current: this.state.timer.current - 15
-            }
+            audioPaused: !this.state.audioPaused
         })
     }
 
     forwardFifteen = () => {
         this.setState({
-            timer: {
-                current: this.state.timer.current + 15
-            }
+            currentTime: Math.floor(this.state.currentTime) + 15
         })
     }
 
-    sliderStarted = () => {
-        alert("Slider Started")
+    backwardFifteen = () => {
+        this.setState({
+            currentTime: Math.floor(this.state.currentTime) - 15
+        })
     }
 
-
     render() {
+        
+        const video = (
+            <Video source={{uri: this.props.audioURL }} paused={this.state.audioPaused} onLoad={this.loadedAudio} onProgress={this.setProgress} ref="sermonElement" />
+        )
+
         return (
             <Modal animationType="slide" visible={this.props.modalVisible}>
                 <StatusBar backgroundColor="#000" barStyle="light-content"/>
                 <SafeAreaView style={{flex: 1, backgroundColor: "#000", flexDirection: "column", justifyContent: "space-between", alignItems: "center"}}>
 
+                
                     {/* Header */}
                     <View style={{ height: 60, width: "100%", alignItems: "center", backgroundColor: "#000", justifyContent: "space-between", flexDirection: "row"}}>
                         <View style={{ width: "20%"}}>
-                            { this.state.audioObject ? <FeatherIconComponent name="share" size={25} style={{textAlign: "center", color: "#fff"}} /> : null }
                         </View>
                         
                         <TouchableOpacity onPress={this.props.close} style={{ width: "20%" }}>
@@ -63,38 +73,32 @@ class AudioModal extends Component {
                     
                     {/* Album */}
                     <View style={{width: "100%", alignItems: "center", justifyContent: "flex-start"}}>
-                        <Image style={{ height: (Dimensions.get('window').width * .8), borderRadius: 10 }} source={require("../../assets/images/album-image.png")} resizeMode="contain" />
+                        <Image style={{ height: (Dimensions.get('window').width * .8), width: (Dimensions.get('window').width * .8), borderRadius: 10 }} source={{uri: this.props.photoURL }} resizeMode="contain" />
                     </View>
 
                     {/* Controls and Info */}
                     <View style={{width: "100%", height: 200, flexDirection: "column", justifyContent: "flex-start", alignItems: "center"}}>
-                        <Text style={{color: "#fff", textAlign: "center", fontSize: 18, fontWeight: "bold", marginBottom: 20}}> { this.state.audioObject.title }</Text>
+                        <Text style={{color: "#fff", textAlign: "center", fontSize: 18, fontWeight: "bold", marginBottom: 20}}> Sermon Title </Text>
                         <Slider
                             style={{width: "80%", height: 40, marginBottom: 10}}
                             minimumValue={0}
-                            maximumValue={100}
-                            value={this.state.timer.current}
+                            maximumValue={this.state.maxTime}
+                            value={this.state.currentTime}
                             minimumTrackTintColor="#FFFFFF"
                             maximumTrackTintColor="#ffffff"
-                            onSlidingStart={this.sliderStarted}
+                            onSlidingComplete={this.sliderComplete}
                         />
-                        <View style={{width: "70%", height: 50, flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
-                            <TouchableOpacity style={{paddingLeft: 20, paddingRight: 20}} onPress={this.backwardFifteen}>
-                                <FeatherIconComponent name="skip-back" size={25} style={{color: "#fff" }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{paddingLeft: 20, paddingRight: 20}} onPress={this.playToggle}>
+                        <View style={{width: "70%", height: 50, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                            <TouchableOpacity style={{paddingLeft: 20, paddingRight: 20}} onPress={this.togglePlay}>
                                 { 
-                                    !this.state.playing ? 
+                                    this.state.audioPaused ? 
                                     <FeatherIconComponent name="play" size={25} style={{color: "#fff" }} /> :
                                     <FeatherIconComponent name="pause" size={25} style={{color: "#fff" }} />
                                 }
                             </TouchableOpacity>
-                            <TouchableOpacity style={{paddingLeft: 20, paddingRight: 20}} onPress={this.forwardFifteen}>
-                                <FeatherIconComponent name="skip-forward" size={25} style={{color: "#fff" }} />
-                            </TouchableOpacity>
                         </View>
                     </View>
-
+                    {video}
                 </SafeAreaView>  
             </Modal> 
         );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,38 +1,74 @@
 import React, { Component } from 'react';
-import { Text, StatusBar, SafeAreaView} from 'react-native';
-import {createStackNavigator } from 'react-navigation';
-import NavbarAudioModalButton from '../components/ui/NavbarAudioModalButton';
+import { View, FlatList, Text, StatusBar, SafeAreaView, TouchableOpacity} from 'react-native';
+import AudioModal from "../components/modals/AudioModal";
+import FeatherIconComponent from "react-native-vector-icons/Feather";
+import { createStackNavigator } from 'react-navigation';
 
 class HomeScreen extends Component{
 
-    state = {
-        modalVisible : false
+    static navigationOptions = {
+        title: "Home"
     }
 
-    static navigationOptions = ({navigation}) => {
-        return {
-            headerRight: (
-                <NavbarAudioModalButton /> 
-            )
-        }
-    } 
+    state = {
+        modalVisible : false,
+        sermons : [
+            {
+                key: "2123123",
+                title: "First Sermon",
+                audioURL: "http://static1.squarespace.com/static/58ac3d315016e1b8ebf53b98/t/5c98e26f652deafd31f3bbb2/1554306701537/NO+FEAR.mp3/original/NO+FEAR.mp3", 
+                photoURL: "https://image.freepik.com/free-vector/gradient-abstract-square-background_1164-1644.jpg"
+            },
+            {
+                key: "12312312lk3j12;3k1",
+                title: "Second Sermon",
+                audioURL: "http://static1.squarespace.com/static/58ac3d315016e1b8ebf53b98/t/5c98e26f652deafd31f3bbb2/1554306701537/NO+FEAR.mp3/original/NO+FEAR.mp3",
+                photoURL: "https://cdn.pixabay.com/photo/2017/09/26/22/23/pixel-2790335_960_720.png"
+            }
+        ]
+    }
+
+    showAudioModal = (audioURL, photoURL) => {
+        this.setState({
+            modalVisible: true,
+            audioURL: audioURL,
+            photoURL: photoURL
+        })
+    }
+
+    closeAudioModal = () => {
+        this.setState({
+            modalVisible: false
+        })
+    }
 
     render() {
+        let audioURL = "http://static1.squarespace.com/static/58ac3d315016e1b8ebf53b98/t/5c98e26f652deafd31f3bbb2/1554306701537/NO+FEAR.mp3/original/NO+FEAR.mp3";
+        
         return (
-            <SafeAreaView style={{ flex: 1, alignItems: "center",justifyContent: "center" }}>
+            <SafeAreaView style={{padding: 15}}>
+                <AudioModal close={this.closeAudioModal} audioURL={this.state.audioURL} photoURL={this.state.photoURL} modalVisible={this.state.modalVisible} />
                 <StatusBar backgroundColor="#eee" barStyle="dark-content"></StatusBar>
-                <Text>Home Screen</Text>
-            </SafeAreaView>
+                
+                <View>
+                    <FlatList data={this.state.sermons} renderItem={({item}) => 
+                        <View style={{padding: 20, backgroundColor: "#eee", marginBottom: 20, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}> 
+                            <View>
+                                <Text> {item.title} </Text>
+                            </View>
+                            <TouchableOpacity onPress={this.showAudioModal.bind(this, item.audioURL, item.photoURL)}>
+                                <FeatherIconComponent name="play-circle" size={25} />
+                            </TouchableOpacity>
+                        </View>
+                    } />
+                </View>
+            </SafeAreaView> 
         )
     }
 }
 
-  
-const HomeStack = createStackNavigator(
-    {
-        Home: HomeScreen
-    }
-)
-
+const HomeStack = createStackNavigator({
+    Home : HomeScreen
+})
 
 export default HomeStack;
